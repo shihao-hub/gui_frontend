2e47777127e9bda68b0d67920e988c5ac2608e14
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-02-11 21:13:30 +0800

    Create mongodb.py

---
+++ projects/nicegui_start_project/nicegui_start_project/models/mongodb.py
@@ -0,0 +1,19 @@
+import mongoengine as engine
+
+DATABASE = "nicegui_start_project"
+DATABASE_ALIAS = f"{DATABASE}_alias"
+
+engine.connect(db=DATABASE, alias=DATABASE_ALIAS, host="localhost", port=27017)
+
+
+class File(engine.Document):
+    filename = engine.StringField(required=True)
+    filepath = engine.StringField(required=True)
+
+    meta = dict(collection="files", db_alias=DATABASE_ALIAS)
+
+    objects: engine.queryset.queryset.QuerySet
+
+
+if __name__ == '__main__':
+    print(type(File.objects))
