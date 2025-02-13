d463359dee7a2551655d2d8aae00f9b08df1899f
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-02-13 19:18:50 +0800

    Update __init__.py

--- projects/nicegui_start_project/nicegui_start_project/pages/components/__init__.py
+++ projects/nicegui_start_project/nicegui_start_project/pages/components/__init__.py
@@ -1,4 +1,5 @@
 from pages.components.todolists import todolists
 from pages.components.file_storage import file_storage
 from pages.components.scheduled_notification import scheduled_notification
+from pages.components.word_notebook import word_notebook
 
