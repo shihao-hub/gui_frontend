a6b6f04ac1c74a20f354e09076b0daf2b092a08e
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-02-26 19:12:03 +0800

    Update configs.py

--- projects/nicegui_start_project/nicegui_start_project/pages/components/countdown_postgraduate_exam/configs.py
+++ projects/nicegui_start_project/nicegui_start_project/pages/components/countdown_postgraduate_exam/configs.py
@@ -3,5 +3,5 @@ from nicegui_start_project.settings import SOURCE_DIR
 PAGE_ICON = "⏳"
 PAGE_TITLE = "考研倒计时"
 PAGE_PATH = "/pages/components/countdown_postgraduate_exam"
-HTML_PATH = f"{SOURCE_DIR}/pages/components/countdown_postgraduate_exam/templates/index.html"
+HTML_PATH = f"{SOURCE_DIR}/pages/components/countdown_postgraduate_exam/static/index.html"  # static/templates/
 VERSION = "1.0.0"
