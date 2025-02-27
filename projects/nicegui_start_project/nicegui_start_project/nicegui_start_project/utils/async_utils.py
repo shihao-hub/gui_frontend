40d08604fa7e4919468ad46eda9137d15986bc53
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-02-27 20:55:25 +0800

    Create async_utils.py

---
+++ projects/nicegui_start_project/nicegui_start_project/nicegui_start_project/utils/async_utils.py
@@ -0,0 +1,13 @@
+__all__ = ["sync_to_async"]
+
+import asyncio
+
+from .mediator import thread_pool
+
+
+async def sync_to_async(func, *args, **kwargs):
+    def sync_code():
+        return func(*args, **kwargs)
+
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(thread_pool.get_pool(), sync_code)
