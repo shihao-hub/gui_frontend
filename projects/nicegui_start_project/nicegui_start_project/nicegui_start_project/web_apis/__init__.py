48c309c44224c55c3806ba9100cc0c3f39b56bcc
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-02-27 20:56:49 +0800

    Update __init__.py

--- projects/nicegui_start_project/nicegui_start_project/nicegui_start_project/web_apis/__init__.py
+++ projects/nicegui_start_project/nicegui_start_project/nicegui_start_project/web_apis/__init__.py
@@ -1,71 +1,60 @@
 __all__ = [
-    "get_users",
-
     "upload_file",
     "download_file",
 ]
 
 import asyncio
 import io
+import json
 import os
 import pprint
 import subprocess
 import traceback
 from typing import Dict, TypedDict, Optional, Any, BinaryIO
 
-from fastapi import UploadFile
-from pydantic import BaseModel
-
 import aiohttp
 from aiohttp import web, FormData
+from aiohttp.client_exceptions import ClientError, ClientResponseError
 from loguru import logger
+from pydantic import BaseModel
 
-from fastapi.responses import StreamingResponse, Response
+from fastapi import UploadFile
+from fastapi.responses import StreamingResponse, Response, JSONResponse
 
-from nicegui_start_project.utils import cached, simple_cache
-
-
-# 这个能不能这样？直接用 Schema/BaseModel 充当对象，实例化并校验和传递
-class GetUsersBodySchema(BaseModel):
-    pass
+from nicegui_start_project.utils import cached, simple_cache, Maybe, Result
 
 
-class GetUsersBody(TypedDict):
-    pass
+class ErrorResponseDict(TypedDict):
+    detail: str
 
 
-def get_users(body: GetUsersBody):
-    pass
-
-
-class UploadFileResponse(TypedDict):
+# 能不能直接用 Schema/BaseModel 充当对象，实例化并校验和传递？而不是 TypedDict
+class UploadFileResponseDict(TypedDict):
     uid: str
 
 
 @cached(expire_seconds=0)
 def _is_port_listening(port):
-    print("is_port_listening is_port_listening is_port_listening")
+    logger.info("call is_port_listening")
     try:
         # Linux 使用 netstat，Windows 使用 netstat
         cmd = ["netstat", "-tuln"] if not os.name == "nt" else ["netstat", "-ano"]
         result = subprocess.run(cmd, capture_output=True, text=True)
         return f":{port} " in result.stdout
     except Exception as e:
-        print(f"Error: {e}")
+        logger.error(f"{e}")
         return False
 
 
 def _check_port_listening(port):
     """ 通过系统命令快速检测（Linux/Windows 兼容） """
-    print(simple_cache)
+    logger.info(simple_cache)
 
     if not _is_port_listening(port):
         raise RuntimeError(f"Port {port} is not listening.")
 
 
-async def upload_file(filename: str, content: BinaryIO) -> UploadFileResponse:
-    _check_port_listening(12001)
-
+async def upload_file(filename: str, content: BinaryIO) -> Result[UploadFileResponseDict, Exception]:
     url = "http://localhost:12001/api/upload"
 
     async def fetch() -> Dict:
@@ -82,40 +71,50 @@ async def upload_file(filename: str, content: BinaryIO) -> UploadFileResponse:
                 return await response.json()  # response 类比 js 的 Promise
 
     try:
+        _check_port_listening(12001)
         data = await fetch()
-        print(f"upload_file: {type(data)}, {data}")
-        return data
+        logger.info(f"upload_file: {type(data)}, {data}")
+        return Result.ok(data)
     except Exception as e:
         logger.error(f"{e}\n{traceback.format_exc()}")
+        return Result.err(e)
 
 
-async def download_file(uid: str):
-    _check_port_listening(12001)
-
+async def download_file(uid: str) -> Result[Response, Dict]:
+    # note: 本函数将 download api 封装，这意味着 download api 发生变化的时候，当前函数也可能受影响！
     url = "http://localhost:12001/api/download"
 
     try:
+        # todo: 12001 去配置文件获取
+        _check_port_listening(12001)
+
         async with aiohttp.ClientSession() as session:
             async with session.get(url, params=dict(uid=uid)) as response:
-                response.raise_for_status()  # 检查 HTTP 响应状态
+                # response.raise_for_status()  # 检查 HTTP 响应状态
                 status_code = response.status
                 headers = response.headers
-                print(pprint.pformat(headers), flush=True)
+                logger.info(pprint.pformat(headers))
                 content = await response.content.read()
-                return Response(status_code=status_code, content=content, headers=headers)
+                if status_code != 200:
+                    try:
+                        return Result.err(content.decode("utf-8"))
+                    except json.JSONDecodeError:
+                        return Result.err({"detail": content})
+                return Result.ok(Response(status_code=status_code, content=content, headers=headers))
     except Exception as e:
         logger.error(f"{e}\n{traceback.format_exc()}")
+        return Result.err({"detail": str(e)})
 
 
 if __name__ == '__main__':
     # async def main():
     #     with open("./settings.py", "rb") as f:
     #         res = await upload_file("default.py", io.BytesIO(f.read()))
-    #     print(res)
+    #     logger.info(res)
     #     res = await download_file(res["uid"])
-    #     print(res)
+    #     logger.info(res)
     #
     #
     # asyncio.run(main())
 
-    print(_check_port_listening(12001))
+    logger.info(_check_port_listening(12001))
