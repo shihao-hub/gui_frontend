3041f75c8cb0a057ddcef065b5dd034c24b45c76
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-02-27 20:54:58 +0800

    Update mongodb_serializer.py

--- projects/nicegui_start_project/nicegui_start_project/nicegui_start_project/commands/mongodb_serializer.py
+++ projects/nicegui_start_project/nicegui_start_project/nicegui_start_project/commands/mongodb_serializer.py
@@ -7,10 +7,12 @@
 
 import argparse
 
+from loguru import logger
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="负责将 mongodb 数据库的数据序列化和反序列化")
     parser.add_argument("action", type=str, choices=["serialize", "deserialize"],
                         help="操作类型，可选值必须为 serialize 或 deserialize")
 
     args = parser.parse_args()
-    print(args.action)
+    logger.info(args.action)
