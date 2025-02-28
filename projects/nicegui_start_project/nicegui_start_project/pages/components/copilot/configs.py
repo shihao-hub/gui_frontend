611c8796ebdee07a9e178ccd10ff7c1488937e18
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-02-28 15:08:19 +0800

    Update configs.py

--- projects/nicegui_start_project/nicegui_start_project/pages/components/copilot/configs.py
+++ projects/nicegui_start_project/nicegui_start_project/pages/components/copilot/configs.py
@@ -3,5 +3,6 @@ PAGE_TITLE = "智能助手"
 PAGE_PATH = "/pages/components/copilot"
 VERSION = "1.0.0"
 
-DEEP_SEEK_MODEL_NAME = "deepseek-chat"  # deepseek-reasoner
+_models = ["deepseek-chat", "deepseek-reasoner"]
+DEEP_SEEK_MODEL_NAME = _models[0]  # 暂时就用 chat 吧，便宜。深度思考的话，前端需要优化，还很贵，建议用其他企业部署的网页！
 DEEP_SEEK_BASE_URL = "https://api.deepseek.com"
