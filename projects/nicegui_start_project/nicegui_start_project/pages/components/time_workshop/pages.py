97e09f0bacdfba6f096d06cd78ea4b4649dae7bc
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-03-06 20:45:14 +0800

    Update pages.py

--- projects/nicegui_start_project/nicegui_start_project/pages/components/time_workshop/pages.py
+++ projects/nicegui_start_project/nicegui_start_project/pages/components/time_workshop/pages.py
@@ -77,3 +77,14 @@ async def time_workshop():
         for elem in ui_tabs:
             with ui.tab_panel(elem.tab) as tab_panel:
                 elem.module.init_widget(tab_panel)
+
+    # create_menu
+    result = ui.label().classes('mr-auto')
+    with ui.button(icon="menu"):
+        with ui.menu() as menu:
+            ui.menu_item('Menu item 1', lambda: result.set_text('Selected item 1'))
+            ui.menu_item('Menu item 2', lambda: result.set_text('Selected item 2'))
+            ui.menu_item('Menu item 3 (keep open)',
+                         lambda: result.set_text('Selected item 3'), auto_close=False)
+            ui.separator()
+            ui.menu_item('Close', on_click=menu.close)
