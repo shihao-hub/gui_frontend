b0cfc5b18fa7a65e80a094cf41ff0455d47025d3
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-03-08 18:01:52 +0800

    Update robustness.py

--- projects/nicegui_start_project/nicegui_start_project/nicegui_start_project/utils/robustness.py
+++ projects/nicegui_start_project/nicegui_start_project/nicegui_start_project/utils/robustness.py
@@ -1,2 +1,11 @@
-class GuaranteeRobustness:
+__all__ = ["GuaranteedRobustness"]
+
+from typing import Tuple, Union, Any
+
+
+def guaranteed_robustness() -> Tuple[bool, Union[str, Any]]:
+    return False, "guaranteed robustness?"
+
+
+class GuaranteedRobustness:
     pass
