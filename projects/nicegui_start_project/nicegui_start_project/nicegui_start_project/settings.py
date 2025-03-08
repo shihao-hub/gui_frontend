454771ef659caa80549a573d24d2672db57010bd
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-03-08 18:02:12 +0800

    Update settings.py

--- projects/nicegui_start_project/nicegui_start_project/nicegui_start_project/settings.py
+++ projects/nicegui_start_project/nicegui_start_project/nicegui_start_project/settings.py
@@ -1,12 +1,15 @@
+import contextlib
 import sqlite3
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Type, Generator, ContextManager
 
 import pymongo
 import redis
 import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Engine as SqlAlchemyEngine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm.session import Session as SqlAlchemySession
+from sqlalchemy.orm.decl_api import DeclarativeMeta as SqlAlchemyDeclarativeMeta
 
 from .mediator import get_configs
 
@@ -34,13 +37,58 @@ COMPONENTS_PACKAGE_NAME = "pages.components"
 class _DatabaseManager:
     """优化成懒加载"""
 
+    # ========================================
+    # make lazy loading to avoid slow startup?
+    # ========================================
+    class _Sqlalchemy:
+        def __init__(self):
+            self._engine: Optional[SqlAlchemyEngine] = None
+            self._Base: Optional[Type[SqlAlchemyDeclarativeMeta]] = None
+            self._Session: Optional[Type[SqlAlchemySession]] = None
+
+        def _get_engine(self):
+            if self._engine is None:
+                self._engine = create_engine(f"sqlite:///{SQLITE_DB_PATH}")
+            return self._engine
+
+        @property
+        def engine(self):
+            return self._get_engine()
+
+        @property
+        def Session(self):  # NOQA
+            if self._Session is None:
+                self._Session = sessionmaker(bind=self._get_engine())
+            return self._Session
+
+        @property
+        def Base(self):  # NOQA
+            if self._Base is None:
+                self._Base = declarative_base()
+            return self._Base
+
+        @contextlib.contextmanager
+        def session_scope(self) -> ContextManager[SqlAlchemySession]:
+            """Provide a transactional scope around a series of operations."""
+            session: SqlAlchemySession = self.Session()
+            try:
+                yield session
+                session.commit()
+            except Exception:
+                session.rollback()
+                raise
+            finally:
+                session.close()
+
+        def create_all(self):
+            self.Base.metadata.create_all(self.engine)
+
     def __init__(self):
         self._sqlite_db: Optional[sqlite3.Connection] = None  # sqlite 最省事，因为是文件数据库
         self._mongodb_db: Optional[pymongo.MongoClient] = None
         self._redis_db: Optional[redis.StrictRedis] = None
 
-        self._sqlalchemy_engine: Optional[sqlalchemy.Engine] = None
-        self._SqlAlchemySession: Optional[sqlalchemy.sessionmaker] = None
+        self.sqlalchemy = self._Sqlalchemy()
 
     @property
     def sqlite_db(self):
@@ -59,23 +107,6 @@ class _DatabaseManager:
         if self._redis_db is None:
             self._redis_db = redis.StrictRedis(host="localhost", port=6379, db=0)
         return self._redis_db
-
-    def _get_sqlalchemy_engine(self):
-        if self._sqlalchemy_engine is None:
-            self._sqlalchemy_engine = create_engine(f"sqlite:///{SQLITE_DB_PATH}")
-        return self._sqlalchemy_engine
-
-    @property
-    def sqlalchemy_engine(self):
-        return self._get_sqlalchemy_engine()
-
-    @property
-    def SqlAlchemySession(self):  # NOQA
-        if self._SqlAlchemySession is None:
-            self._SqlAlchemySession = sessionmaker(bind=self._get_sqlalchemy_engine())
-        return self._SqlAlchemySession
-
-    # def create_(self):
 
 
 database_manager = _DatabaseManager()
