336e085ae3100e56a533f7c98f9af684751f9650
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-03-13 19:38:26 +0800

    Update mediator.py

--- projects/nicegui_start_project/nicegui_start_project/nicegui_start_project/utils/mediator.py
+++ projects/nicegui_start_project/nicegui_start_project/nicegui_start_project/utils/mediator.py
@@ -1,20 +1,11 @@
 __all__ = ["thread_pool", "SingletonMeta"]
 
-import threading
-
 
 def thread_pool():
     from .thread_utils import thread_pool as thread_pool_alias
     return thread_pool_alias
 
 
-class SingletonMeta(type):
-    _instances = {}  # fixme: 这个得弱引用吧？即若某个对象只被 _instances 持有，垃圾回收器会回收它
-    _lock = threading.Lock()
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            with cls._lock:
-                if cls not in cls._instances:
-                    cls._instances[cls] = super().__call__(*args, **kwargs)
-        return cls._instances[cls]
+def SingletonMeta():  # NOQA
+    from .singleton import SingletonMeta as SingletonMeta_alias
+    return SingletonMeta_alias
