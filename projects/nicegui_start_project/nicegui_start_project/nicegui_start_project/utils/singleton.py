cba99bf4b243d3a542d9a7fa160cfd30181a70ac
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-03-13 19:38:50 +0800

    Create singleton.py

---
+++ projects/nicegui_start_project/nicegui_start_project/nicegui_start_project/utils/singleton.py
@@ -0,0 +1,13 @@
+import threading
+
+
+class SingletonMeta(type):
+    _instances = {}  # fixme: 这个得弱引用吧？即若某个对象只被 _instances 持有，垃圾回收器会回收它
+    _lock = threading.Lock()
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            with cls._lock:
+                if cls not in cls._instances:
+                    cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
